992456d9e5232299afba95a3a686a8dec32104f9
David Simansky <dsimansk@redhat.com>
2018-06-01 14:22:16 +0200

    feat: Use floating tag to reference product images

--- fuse_ignite_config.sh
+++ fuse_ignite_config.sh
@@ -6,10 +6,10 @@ git_syndesis="1.3.10"
 git_fuse_ignite_install="1.3.10-CR"
 
 # Tags used for the productised images
-tag_server="1.0-6"
-tag_ui="1.0-5"
-tag_meta="1.0-5"
-tag_s2i="1.0-5"
+tag_server="1.0"
+tag_ui="1.0"
+tag_meta="1.0"
+tag_s2i="1.0"
 
 # Maven repositories
 maven_repo_redhat="https://maven.repository.redhat.com/ga/"
@@ -21,4 +21,3 @@ registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
 
 # Official:
 # registry="registry.access.redhat.com"
-
