bc5abb521556cce76ee185496c36140c84e7f109
Roland Huß <roland@ro14nd.de>
2018-09-06 12:30:07 +0200

    update versions

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,13 +1,13 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.4.2"
+git_fuse_online_install="1.4.8"
 
 # Tags used for the productised images
-tag_server="1.1-4"
-tag_ui="1.1-3"
-tag_meta="1.1-2"
-tag_s2i="1.1-3"
-tag_upgrade="1.1-2"
-tag_operator="1.1-1"
+tag_server="1.1-13"
+tag_ui="1.1-8"
+tag_meta="1.1-12"
+tag_s2i="1.1-10"
+tag_upgrade="1.1-10"
+tag_operator="1.1-6"
 
 # Docker repository for productised images
 repository="fuse7"
