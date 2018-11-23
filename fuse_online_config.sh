c3a471282f0ae9a24576d8d8a89139243b9d6d6e
Roland Huß <roland@ro14nd.de>
2018-11-23 19:14:06 +0100

    updated version to 1.5.7

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,5 +1,5 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.5.6"
+git_fuse_online_install="1.5.7"
 
 # Tags used for the productised images
 tag_server="1.2-4"
