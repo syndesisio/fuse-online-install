f8fb661a149719e6125da362cc2f50ddc029b272
Roland Huß <roland@ro14nd.de>
2018-11-23 19:14:18 +0100

    [1.5.7]: Update release config for 1.5.7

--- install_ocp.sh
+++ install_ocp.sh
@@ -7,7 +7,7 @@
 
 # ================
 # Tag updated by release script
-TAG=1.5.6
+TAG=1.5.7
 # ================
 
 # Minimal version for OC
--- update_ocp.sh
+++ update_ocp.sh
@@ -5,7 +5,7 @@
 
 # ================
 # Target version to update to
-TAG=1.5.6
+TAG=1.5.7
 # ================
 
 # Minimal version for OC
