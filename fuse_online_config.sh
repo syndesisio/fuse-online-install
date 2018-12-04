9812c78cc8fa4b767086e214e78c797858775c5b
Stan Lewis <slewis@fusesource.com>
2018-12-04 09:50:49 -0500

    Prepare for final release

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,5 +1,5 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.5.11"
+git_fuse_online_install="1.5.12"
 
 # Tags used for the productised images
 tag_server="1.2-5"
@@ -13,7 +13,7 @@ tag_operator="1.2-7"
 repository="fuse7"
 
 # Test:
-registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
+# registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
 
 # Official:
-# registry="registry.access.redhat.com"
+registry="registry.access.redhat.com"
