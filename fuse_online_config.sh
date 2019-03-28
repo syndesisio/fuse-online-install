baa8d2af0ff119cb8ec34563ba47929b5c66b5a5
Van Halbert <vhalbert@redhat.com>
2019-03-28 10:52:23 -0400

    Rebuild for Fuse 7.3 CR1

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,12 +1,12 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.6.x"
+git_fuse_online_install="1.6.0"
 
 # Tags used for the productised images
-tag_server="1.3"
-tag_ui="1.3"
-tag_meta="1.3"
-tag_s2i="1.3"
-tag_upgrade="1.3"
+tag_server="1.3-4"
+tag_ui="1.3-4"
+tag_meta="1.3-4"
+tag_s2i="1.3-4"
+tag_upgrade="1.3-2"
 tag_operator="1.3-2"
 
 
