06db0f38afdf507accc70e5eac2d3a04fefb1796
Van Halbert <vhalbert@redhat.com>
2019-04-01 08:41:37 -0400

    [1.6.5]: Update release config for 1.6.5

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,13 +1,13 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.6.4"
+git_fuse_online_install="1.6.5"
 
 # Tags used for the productised images
 tag_server="1.3-4"
 tag_ui="1.3-4"
 tag_meta="1.3-4"
 tag_s2i="1.3-4"
-tag_upgrade="1.3-4"
-tag_operator="1.3-4"
+tag_upgrade="1.3-5"
+tag_operator="1.3-5"
 
 
 # Docker repository for productised images
