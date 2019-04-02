8eb8b6083b27044225d82dc2bed2b8bc14a15986
Van Halbert <vhalbert@redhat.com>
2019-04-02 07:53:26 -0400

    [1.6.5]: Update release config for 1.6.5

--- install_ocp.sh
+++ install_ocp.sh
@@ -7,7 +7,7 @@
 
 # ================
 # Tag updated by release script
-TAG=1.5.1
+TAG=1.6.5
 # ================
 
 # Minimal version for OC
--- update_ocp.sh
+++ update_ocp.sh
@@ -5,7 +5,7 @@
 
 # ================
 # Target version to update to
-TAG=1.5.1
+TAG=1.6.5
 # ================
 
 # Minimal version for OC
