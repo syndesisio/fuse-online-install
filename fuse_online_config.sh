6fee9c9af065e95817fefb92cb75adb3b5e02f8e
Luis Garcia Acosta <lgarciaac@gmail.com>
2019-04-02 17:17:20 +0200

    Revert images to registry wothout auth

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -2,19 +2,19 @@
 git_fuse_online_install="1.6.4"
 
 # Tags used for the productised images
-tag_server="1.2-12"
-tag_ui="1.2-6"
-tag_meta="1.2-12"
-tag_s2i="1.2-9"
-tag_upgrade="1.2-18"
-tag_operator="1.2-13"
+tag_server="1.3-4"
+tag_ui="1.3-4"
+tag_meta="1.3-4"
+tag_s2i="1.3-4"
+
+tag_upgrade="1.3-4"
+tag_operator="1.3-4"
 
 # Docker repository for productised images
 repository="fuse7"
 
 # Test:
-#registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
+registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
 
 # Official:
-
-registry="registry.redhat.io"
+#registry="registry.redhat.io"
