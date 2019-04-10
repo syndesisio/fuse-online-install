5ee832fcfa63d34ee762781278a1f802fe809d5c
nferraro <ni.ferraro@gmail.com>
2019-04-10 19:59:10 +0200

    Fix #86: use productized version of camel k

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -9,13 +9,15 @@ tag_s2i="1.2-9"
 tag_upgrade="1.2-18"
 tag_operator="1.2-13"
 tag_postgres_exporter="1.3"
+tag_camel_k="1.3"
 
 # Docker repository for productised images
 repository="fuse7"
 
 # Test:
 #registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
+#maven_repository="https://origin-repository.jboss.org/nexus/content/groups/ea@id=redhat.ea"
 
 # Official:
-
 registry="registry.redhat.io"
+maven_repository="https://maven.repository.redhat.com/ga@id=redhat.ga"
