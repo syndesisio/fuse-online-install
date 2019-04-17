1e5351f617d0aacc8176f9cfc9cc0c4b0550a1e1
Van Halbert <vhalbert@redhat.com>
2019-04-17 09:11:54 -0400

    replicating config changes to template

--- fuse_online_config_template.sh
+++ fuse_online_config_template.sh
@@ -3,15 +3,21 @@
 # Tag for release. Update this before running release.sh
 git_fuse_online_install="version.fuse.online"
 
+# Image Streams to be updated
+imagestreams="server ui meta s2i postgres_exporter"
+
 # Tags used for the productised images
 tag_server="version.tag.server"
 tag_ui="version.tag.ui"
 tag_meta="version.tag.meta"
 tag_s2i="version.tag.s2i"
+tag_postgres_exporter="version.tag.postgres"
 
 tag_upgrade="version.tag.upgrade"
 tag_operator="version.tag.operator"
-tag_postgres_exporter="version.tag.postgres"
+
+tag_camel_k="version.tag.camel.k"
+tag_java_base_image="version.tag.java.base.image"
 
 
 # Docker repository for productised images
@@ -19,7 +25,7 @@ repository="repo.name"
 
 # Test:
 registry="registry.name"
-maven_repository="maven.repository"
+maven_repository="maven.repository.name"
 
 # Official:
 # registry="registry.access.redhat.com"
