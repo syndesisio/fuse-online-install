71b9786f3704acce927188e34821b7ffc412d49b
Fuse Jenkins <fuse-infra-team@redhat.com>
2019-05-01 13:42:47 +0000

    [1.6.15]: Update release config for 1.6.15

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,28 +1,28 @@
+# This template is used in Jenkins to enable property substitution at runtime
+
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.6.5"
+git_fuse_online_install="1.6.15"
 
 # Tags used for the productised images
-tag_server="1.3-4"
-tag_ui="1.3-4"
-tag_meta="1.3-4"
-tag_s2i="1.3-4"
-tag_upgrade="1.3-5"
-tag_operator="1.3-5"
+tag_server="1.3-16"
+tag_ui="1.3-10"
+tag_meta="1.3-16"
+tag_s2i="1.3-16"
 
-tag_postgres_exporter="1.3"
-tag_camel_k="1.3"
-tag_java_base_image="1.3"
+tag_upgrade="1.3-9"
+tag_operator="1.3-9"
+tag_postgres_exporter="1.3-4"
 
-tag_upgrade="1.3-5"
-tag_operator="1.3-5"
+tag_camel_k="1.3-4"
+tag_java_base_image="1.3-10"
 
 # Docker repository for productised images
 repository="fuse7"
 
 # Test:
-registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
-maven_repository="https://origin-repository.jboss.org/nexus/content/groups/ea@id=redhat.ea"
+registry="registry.access.redhat.com"
+maven_repository="https://maven.repository.redhat.com/ga@id=redhat.ga"
 
 # Official:
-#registry="registry.redhat.io"
-#maven_repository="https://maven.repository.redhat.com/ga@id=redhat.ga"
+# registry="registry.access.redhat.com"
+# maven_repository="https://maven.repository.redhat.com/ga@id=redhat.ga"
