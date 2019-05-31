0fda0882169d74ff1b3a651e9921bfdd27179fbf
Van Halbert <vhalbert@redhat.com>
2019-05-31 13:31:52 -0400

    [1.6.21]: Update release config for 1.6.21

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,23 +1,23 @@
 # This template is used in Jenkins to enable property substitution at runtime
 
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.6.20"
+git_fuse_online_install="1.6.21"
 
 # Tags used for the productised images
-tag_server="1.3-16"
-tag_ui="1.3-10"
-tag_meta="1.3-16"
-tag_s2i="1.3-16"
+tag_server="1.3-19"
+tag_ui="1.3-12"
+tag_meta="1.3-19"
+tag_s2i="1.3-19"
 
-tag_upgrade="1.3-9"
-tag_operator="1.3-9"
-tag_postgres_exporter="1.3-4"
+tag_upgrade="1.3-12"
+tag_operator="1.3-13"
+tag_postgres_exporter="1.3-5"
 
-tag_camel_k="1.3-4"
-tag_java_base_image="1.3-10"
+tag_camel_k="1.3-7"
+tag_java_base_image="1.3-12"
 
 # Docker repository for productised images
 repository="fuse7"
 
-registry="registry.redhat.io"
-maven_repository="https://maven.repository.redhat.com/ga@id=redhat.ga"
+registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
+maven_repository="https://origin-repository.jboss.org/nexus/content/groups/ea@id=redhat.ea"
