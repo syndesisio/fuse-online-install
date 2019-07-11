e61057585f78ea98bac2245bf352df0ace3c5f28
Van Halbert <vhalbert@redhat.com>
2019-07-11 08:58:11 -0400

    [1.7.4]: Update release config for 1.7.4

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,23 +1,23 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.7.3"
+git_fuse_online_install="1.7.4"
 
 # Image Streams to be updated
 imagestreams="server ui meta s2i postgres_exporter komodo"
 
 # Tags used for the productised images
-tag_server="1.4-2"
-tag_ui="1.4-1"
-tag_meta="1.4-1"
-tag_s2i="1.4-1"
+tag_server="1.4-5"
+tag_ui="1.4-2"
+tag_meta="1.4-4"
+tag_s2i="1.4-4"
 
-tag_upgrade="1.4-1"
-tag_operator="1.4-3"
+tag_upgrade="1.4-2"
+tag_operator="1.4-4"
 tag_postgres_exporter="1.4-1"
 
 tag_camel_k="1.4-1"
-tag_java_base_image="1.4-6"
+tag_java_base_image="1.4-7"
 
-tag_komodo="1.4-1"
+tag_komodo="1.4-3"
 
 # Docker repository for productised images
 repository="fuse7"
