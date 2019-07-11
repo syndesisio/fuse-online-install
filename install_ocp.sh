f870a1dd0c6d55eed7cbed7b716b2d670d94f51a
Van Halbert <vhalbert@redhat.com>
2019-07-11 08:58:11 -0400

    [1.7.4]: Update release config for 1.7.4

--- install_ocp.sh
+++ install_ocp.sh
@@ -7,7 +7,7 @@
 
 # ================
 # Tag updated by release script
-TAG=1.7.3
+TAG=1.7.4
 # ================
 
 # Minimal version for OC
--- update_ocp.sh
+++ update_ocp.sh
@@ -5,7 +5,7 @@
 
 # ================
 # Target version to update to
-TAG=1.7.3
+TAG=1.7.4
 # ================
 
 # Minimal version for OC
