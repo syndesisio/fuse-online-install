12685021ef6886b7330092b6e660a4bf36e9ebeb
Luis Garcia Acosta <lgarciaac@gmail.com>
2019-07-17 17:04:18 +0200

    Create resource for operator hub

--- release.sh
+++ release.sh
@@ -228,10 +228,16 @@ create_resources() {
                --entrypoint bash \
                $registry/$repository/fuse-online-operator:$tag_operator \
                -c "cp /conf/syndesis-template.yaml /resources/fuse-online-template.yml"
-               
+
     echo "==== Patch template removing camel-k related resources"
     sed -i.bak '/# START:CAMEL-K/,/# END:CAMEL-K/d' $topdir/resources/fuse-online-template.yml
     rm -f $topdir/resources/fuse-online-template.yml.bak               
+
+    echo "==== Create full template for operatorhub integration"
+    cat $topdir/resources/fuse-online-template.yml \
+      > $topdir/resources/fuse-online-template-oh.yml
+    sed -e '1,2d' $topdir/resources/fuse-online-image-streams.yml \
+      >> $topdir/resources/fuse-online-template-oh.yml
 }
 
 release() {
