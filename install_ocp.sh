12cd73e5d574ab77e033cf93a387b8c541371c08
Heiko Braun <ike.braun@googlemail.com>
2019-07-24 17:52:05 +0200

    [1.7.20]: Update release config for 1.7.20

--- install_ocp.sh
+++ install_ocp.sh
@@ -7,7 +7,7 @@
 
 # ================
 # Tag updated by release script
-TAG=1.7.19
+TAG=1.7.20
 # ================
 
 # Minimal version for OC
--- update_ocp.sh
+++ update_ocp.sh
@@ -5,7 +5,7 @@
 
 # ================
 # Target version to update to
-TAG=1.7.19
+TAG=1.7.20
 # ================
 
 # Minimal version for OC
