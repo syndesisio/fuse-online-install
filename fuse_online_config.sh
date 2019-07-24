99724957586e50c148336db73598307fc4ff7ad3
Heiko Braun <ike.braun@googlemail.com>
2019-07-24 17:52:05 +0200

    [1.7.20]: Update release config for 1.7.20

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,5 +1,5 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.7.19"
+git_fuse_online_install="1.7.20"
 
 # Image Streams to be updated
 imagestreams="server ui meta s2i postgres_exporter komodo"
