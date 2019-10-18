d4f36e9fbcf04e947c3555954e7b5604f9e6ee0c
nferraro <ni.ferraro@gmail.com>
2019-10-18 12:28:26 +0200

    ENTESB-11837: extract binaries from productization and publish them

--- common_config.sh
+++ common_config.sh
@@ -15,16 +15,22 @@ CURRENT_OS=$(get_current_os)
 BINARY_FILE_EXTENSION=$(get_executable_file_extension)
 OC_MIN_VERSION=3.9.0
 
+COMMON_RELEASE_GIT_ORG=jboss-fuse
+COMMON_RELEASE_GIT_REPO=fuse-clients
+BIN_TAG_PREFIX=
+
 # Camel K settings
-CAMEL_K_VERSION=0.3.4
+CAMEL_K_VERSION=$TAG_FUSE_ONLINE_INSTALL
 CAMEL_K_BINARY=kamel
-CAMEL_K_GIT_ORG=jboss-fuse
-CAMEL_K_GIT_REPO=camel-k
-CAMEL_K_DOWNLOAD_URL=https://github.com/${CAMEL_K_GIT_ORG}/${CAMEL_K_GIT_REPO}/releases/download/${CAMEL_K_VERSION}/camel-k-client-${CAMEL_K_VERSION}-${CURRENT_OS}-64bit.tar.gz
+CAMEL_K_GIT_ORG=$COMMON_RELEASE_GIT_ORG
+CAMEL_K_GIT_REPO=$COMMON_RELEASE_GIT_REPO
+CAMEL_K_DOWNLOAD_URL=https://github.com/${CAMEL_K_GIT_ORG}/${CAMEL_K_GIT_REPO}/releases/download/${BIN_TAG_PREFIX}${CAMEL_K_VERSION}/camel-k-client-${CAMEL_K_VERSION}-${CURRENT_OS}-64bit.tar.gz
+CAMEL_K_IMAGE=registry-proxy.engineering.redhat.com/rh-osbs/fuse7-tech-preview-fuse-camel-k:1.5-12
 
 # Syndesis settings
-SYNDESIS_VERSION=1.8.1-20190920
-SYNDESIS_BINARY=syndesis
-SYNDESIS_GIT_ORG=nicolaferraro
-SYNDESIS_GIT_REPO=syndesis
-SYNDESIS_DOWNLOAD_URL=https://github.com/${SYNDESIS_GIT_ORG}/${SYNDESIS_GIT_REPO}/releases/download/${SYNDESIS_VERSION}/syndesis-${SYNDESIS_VERSION}-${CURRENT_OS}-64bit.tgz
+SYNDESIS_VERSION=$TAG_FUSE_ONLINE_INSTALL
+SYNDESIS_BINARY=syndesis-operator
+SYNDESIS_GIT_ORG=$COMMON_RELEASE_GIT_ORG
+SYNDESIS_GIT_REPO=$COMMON_RELEASE_GIT_REPO
+SYNDESIS_DOWNLOAD_URL=https://github.com/${SYNDESIS_GIT_ORG}/${SYNDESIS_GIT_REPO}/releases/download/${BIN_TAG_PREFIX}${SYNDESIS_VERSION}/syndesis-${SYNDESIS_VERSION}-${CURRENT_OS}-64bit.tar.gz
+SYNDESIS_IMAGE=registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-online-operator:1.5-7
