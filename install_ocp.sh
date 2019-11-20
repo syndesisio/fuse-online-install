1e61ece898eb25f4bf054e2b5721e375ba380e92
Luis Garcia Acosta <lgarciaac@gmail.com>
2019-11-20 17:02:39 +0100

    Secret linking and prepare for 1.8.13

--- install_ocp.sh
+++ install_ocp.sh
@@ -335,6 +335,9 @@ set -e
 echo "Deploying Syndesis operator"
 $SYNDESIS_CLI install operator
 
+result=$(oc secrets link syndesis-operator syndesis-pull-secret --for=pull >$ERROR_FILE 2>&1)
+check_error $result
+
 if [ $(hasflag --camel-k) ]; then
     echo "Deploying Camel-K operator"
     $KAMEL_CLI install --skip-cluster-setup $(readopt --camel-k-options)
