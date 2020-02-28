ad37c434e5fc7f51672f2760774351756de8ba5d
heiko-braun <ike.braun@googlemail.com>
2020-02-28 07:31:53 +0100

    [1.9.4]: Update release config for 1.9.4

--- common_config.sh
+++ common_config.sh
@@ -5,7 +5,7 @@
 source $BASEDIR/base_functions.sh
 
 # Tag for release. Update this before running release.sh
-TAG_FUSE_ONLINE_INSTALL=1.9.1
+TAG_FUSE_ONLINE_INSTALL=1.9.4
 
 # Fuse minor version (update it manually)
 TAG=1.9
