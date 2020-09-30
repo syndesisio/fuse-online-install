9fff547d37b449b25e0215b326c3dd7c2963f131
Matej Králik <mkralik@redhat.com>
2020-09-30 15:32:19 +0200

    [ENTESB-14342]: Remove Camel-K 0.3.4 runtime from Syndesis (#251)

--- install_ocp.sh
+++ install_ocp.sh
@@ -95,10 +95,6 @@ popd > /dev/null
 SYNDESIS_CLI=$(get_syndesis_bin)
 check_error $SYNDESIS_CLI
 
-# Download binary files
-KAMEL_CLI=$(get_kamel_bin)
-check_error $KAMEL_CLI
-
 display_usage() {
   cat <<EOT
 Fuse Online Installation Tool for OCP
