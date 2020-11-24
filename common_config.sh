94961b31cfa0acf8f466d044e037af2512dee957
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2020-11-24 14:29:37 +0000

    [1.11.4]: Update release config for 1.11.4

--- common_config.sh
+++ common_config.sh
@@ -5,7 +5,7 @@
 source "$BASEDIR/base_functions.sh"
 
 # Tag for release. Update this before running release.sh
-TAG_FUSE_ONLINE_INSTALL=1.11.3
+TAG_FUSE_ONLINE_INSTALL=1.11.4
 
 # Fuse minor version (update it manually)
 TAG=1.11
