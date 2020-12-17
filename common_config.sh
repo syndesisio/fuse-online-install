4812b826959746faaaa92af818a8f07ead702aa7
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2020-12-17 16:48:45 +0000

    Updates the config with the newly location of the final image
    * No need to release from this as this is the same image as that used for
      the 1.11.10 tag release.

--- common_config.sh
+++ common_config.sh
@@ -25,4 +25,4 @@ SYNDESIS_BINARY=syndesis-operator
 SYNDESIS_GIT_ORG=$COMMON_RELEASE_GIT_ORG
 SYNDESIS_GIT_REPO=$COMMON_RELEASE_GIT_REPO
 SYNDESIS_DOWNLOAD_URL=https://github.com/${SYNDESIS_GIT_ORG}/${SYNDESIS_GIT_REPO}/releases/download/${BIN_TAG_PREFIX}${SYNDESIS_VERSION}/syndesis-${SYNDESIS_VERSION}-${CURRENT_OS}-64bit.tar.gz
-SYNDESIS_IMAGE=registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-online-operator:1.8
+SYNDESIS_IMAGE=registry.redhat.io/fuse7/fuse-online-operator:1.8
