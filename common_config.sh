3f39e0385993fa9ce38a5c3b98b1478ab7df4c6f
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2022-03-04 13:06:26 +0000

    [1.13.5]: Update release config for 1.13.5
      * Release for respin 1.10.1

--- common_config.sh
+++ common_config.sh
@@ -5,7 +5,7 @@
 source "$BASEDIR/base_functions.sh"
 
 # Tag for release. Update this before running release.sh
-TAG_FUSE_ONLINE_INSTALL=1.13.4
+TAG_FUSE_ONLINE_INSTALL=1.13.5
 
 # Fuse minor version (update it manually)
 TAG=1.13
